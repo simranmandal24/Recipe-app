45d0e06004e294ad32361af09fbce97c4f796615
simranmandal24 <simranmandal24@navgurukul.org>
2026-02-02 12:41:40 +0530

    dist deleted

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import RecipeModal from "./RecipeModal";
 const App = () => {
   const [recipes, setRecipes] = useState([]);
   const [search, setSearch] = useState("");
-  const [query, setQuery] = useState("chicken");
+  const [query, setQuery] = useState("cake");
   const [selectedRecipe, setSelectedRecipe] = useState(null);
 
   useEffect(() => {
